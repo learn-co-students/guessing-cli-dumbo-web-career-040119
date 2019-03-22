65f74e691e3b6aa907d913253a91ac11b2a90311
Mallory Woods <mallorywoods@Mallorys-iMac.lan>
2019-03-22 16:26:31 -0400

    Done

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,19 @@
-# Code your solution here!
+def run_guessing_game
+  i = 0
+  while i = i
+    puts "Guess a number between 1 and 6."
+    user_response = gets
+    computer_guess = rand
+
+    if "#{user_response}" == "exit"
+      puts "Goodbye!"
+      break
+
+    elsif "#{user_response}" == "#{computer_guess}"
+      puts "You guessed the correct number!"
+
+    else
+      puts "The computer guessed #{computer_guess}."
+    end
+  end
+end
